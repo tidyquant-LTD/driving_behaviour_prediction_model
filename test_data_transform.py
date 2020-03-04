cb6f8275df1ab056672adbe6dee5fc017a8b4c3d
Yana Hontarenko <y.hontarenko@spd-ukraine.com>
2020-03-04 15:22:53 +0200

    Edit scripts for testing files transform and predict on them

--- test_data_transform.py
+++ test_data_transform.py
@@ -1,6 +1,28 @@
+import numpy as np
 import os
 import pandas as pd
+
 from copy import copy
+from scipy.signal import butter, lfilter
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
+def roll_column_with_duplicate(column):
+    result = np.roll(column, 1)
+    result[0] = result[1]
+    return result
 
 listdir = os.listdir(os.path.join('data', 'test'))
 
@@ -23,5 +45,53 @@ for folder in listdir:
     z_temp = copy(data_acc_gyr['z_gyroscope'].values)
     data_acc_gyr['y_gyroscope'] = z_temp
     data_acc_gyr['z_gyroscope'] = y_temp
+
+    data_acc = data_acc.drop(["time"], axis=1)
+    data_acc_gyr = data_acc_gyr.drop(["time"], axis=1)
+
     data_acc.to_csv(os.path.join('data', 'test', 'transformed', f"{folder}_accelerometer.csv"), index=False)
     data_acc_gyr.to_csv(os.path.join('data', 'test', 'transformed', f"{folder}_accelerometer_gyroscope.csv"), index=False)
+
+    data_acc['x_accelerometer'] = butter_lowpass_filter(data_acc['x_accelerometer'].values, 2, 1000)
+    data_acc['y_accelerometer'] = butter_lowpass_filter(data_acc['y_accelerometer'].values, 2, 1000)
+    data_acc['z_accelerometer'] = butter_lowpass_filter(data_acc['z_accelerometer'].values, 2, 1000)
+    data_gyr["x_gyroscope"] = butter_lowpass_filter(data_gyr['x_gyroscope'].values, 2, 1000)
+    data_gyr["y_gyroscope"] = butter_lowpass_filter(data_gyr['y_gyroscope'].values, 2, 1000)
+    data_gyr["z_gyroscope"] = butter_lowpass_filter(data_gyr['z_gyroscope'].values, 2, 1000)
+
+    data_acc["mean_window_x_accelerometer"] = data_acc["x_accelerometer"].rolling(8, min_periods=1).mean()
+    data_acc["mean_window_y_accelerometer"] = data_acc["y_accelerometer"].rolling(8, min_periods=1).mean()
+    data_acc["mean_window_z_accelerometer"] = data_acc["z_accelerometer"].rolling(8, min_periods=1).mean()
+    data_gyr["mean_window_x_gyroscope"] = data_gyr["x_gyroscope"].rolling(8, min_periods=1).mean()
+    data_gyr["mean_window_y_gyroscope"] = data_gyr["y_gyroscope"].rolling(8, min_periods=1).mean()
+    data_gyr["mean_window_z_gyroscope"] = data_gyr["z_gyroscope"].rolling(8, min_periods=1).mean()
+
+    data_acc["std_window_x_accelerometer"] = data_acc["x_accelerometer"].rolling(8, min_periods=1).std()
+    data_acc["std_window_y_accelerometer"] = data_acc["y_accelerometer"].rolling(8, min_periods=1).std()
+    data_acc["std_window_z_accelerometer"] = data_acc["z_accelerometer"].rolling(8, min_periods=1).std()
+    data_gyr["std_window_x_gyroscope"] = data_gyr["x_gyroscope"].rolling(8, min_periods=1).std()
+    data_gyr["std_window_y_gyroscope"] = data_gyr["y_gyroscope"].rolling(8, min_periods=1).std()
+    data_gyr["std_window_z_gyroscope"] = data_gyr["z_gyroscope"].rolling(8, min_periods=1).std()
+
+    data_acc["median_window_x_accelerometer"] = data_acc["x_accelerometer"].rolling(8, min_periods=1).median()
+    data_acc["median_window_y_accelerometer"] = data_acc["y_accelerometer"].rolling(8, min_periods=1).median()
+    data_acc["median_window_z_accelerometer"] = data_acc["z_accelerometer"].rolling(8, min_periods=1).median()
+    data_gyr["median_window_x_gyroscope"] = data_gyr["x_gyroscope"].rolling(8, min_periods=1).median()
+    data_gyr["median_window_y_gyroscope"] = data_gyr["y_gyroscope"].rolling(8, min_periods=1).median()
+    data_gyr["median_window_z_gyroscope"] = data_gyr["z_gyroscope"].rolling(8, min_periods=1).median()
+
+    data_acc["tendency_window_x_accelerometer"] = roll_column_with_duplicate(data_acc["mean_window_x_accelerometer"].values) / data_acc["mean_window_x_accelerometer"]
+    data_acc["tendency_window_y_accelerometer"] = roll_column_with_duplicate(data_acc["mean_window_y_accelerometer"].values) / data_acc["mean_window_y_accelerometer"]
+    data_acc["tendency_window_z_accelerometer"] = roll_column_with_duplicate(data_acc["mean_window_z_accelerometer"].values) / data_acc["mean_window_z_accelerometer"]
+    data_gyr["tendency_window_x_gyroscope"] = roll_column_with_duplicate(data_gyr["mean_window_x_gyroscope"].values) / data_gyr["mean_window_x_gyroscope"]
+    data_gyr["tendency_window_y_gyroscope"] = roll_column_with_duplicate(data_gyr["mean_window_y_gyroscope"].values) / data_gyr["mean_window_y_gyroscope"]
+    data_gyr["tendency_window_z_gyroscope"] = roll_column_with_duplicate(data_gyr["mean_window_z_gyroscope"].values) / data_gyr["mean_window_z_gyroscope"]
+
+    data_acc = data_acc.fillna(method="bfill")
+    data_gyr = data_gyr.fillna(method="bfill")
+
+    data_acc = data_acc.drop(["x_accelerometer", "y_accelerometer", "z_accelerometer"], axis=1)
+    data_acc.to_csv(os.path.join('data', 'test', 'transformed', f"{folder}_accelerometer_features.csv"), index=False)
+    data = pd.concat([data_acc, data_gyr], axis=1)
+    data = data.drop(["time", 'x_gyroscope', 'y_gyroscope', 'z_gyroscope'], axis=1)
+    data.to_csv(os.path.join('data', 'test', 'transformed', f"{folder}_accelerometer_gyroscope_features.csv"), index=False)
--- test_predict.py
+++ test_predict.py
@@ -1,5 +1,22 @@
 import os
-import subprocess
+import pandas as pd
+
+from scipy.signal import butter, lfilter
+from predictor import Predictor
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
 
 listdir = os.listdir(os.path.join('data', 'test', 'transformed'))
 
@@ -7,14 +24,47 @@ if not os.path.exists(os.path.join('data', 'test', 'predict')):
     os.makedirs(os.path.join('data', 'test', 'predict'))
     os.makedirs(os.path.join('data', 'test', 'predict', 'linear'))
     os.makedirs(os.path.join('data', 'test', 'predict', 'non-linear'))
+    os.makedirs(os.path.join('data', 'test', 'predict', 'linear-features'))
+    os.makedirs(os.path.join('data', 'test', 'predict', 'non-linear-features'))
 
-accelerometer_files = [file for file in listdir if "gyroscope" not in file]
-accelerometer_gyroscope_files = [file for file in listdir if "gyroscope" in file]
+accelerometer_files = [file for file in listdir if "gyroscope" not in file and "features" not in file]
+accelerometer_gyroscope_files = [file for file in listdir if "gyroscope" in file and "features" not in file]
+accelerometer_feature_files = [file for file in listdir if "gyroscope" not in file and "features" in file]
+accelerometer_gyroscope_features_files = [file for file in listdir if "gyroscope" in file and "features" in file]
 
+predictor = Predictor()
 for file in accelerometer_files:
-    subprocess.call(f"venv/bin/python predict.py --model-file models/svc.pcl --file {os.path.join('data', 'test', 'transformed', file)} --saving-path {os.path.join('data', 'test', 'predict', 'linear')} --output-filename {file[:-4]}", shell=True)
-    subprocess.call(f"venv/bin/python predict.py --model-file models/random_forest.pcl --model-type non-linear --file {os.path.join('data', 'test', 'transformed', file)} --saving-path {os.path.join('data', 'test', 'predict', 'non-linear')} --output-filename {file[:-4]}", shell=True)
+    data = pd.read_csv(os.path.join('data', 'test', 'transformed', file))
+
+    saving_path = os.path.join('data', 'test', 'predict', 'linear', file)
+    predictor.predict_and_save(data=data, saving_path=saving_path, linear=True, model_filename="linear-accelerometer.pcl", features="simple", filtering=butter_lowpass_filter, cutoff=2, fs=1000)
+
+    saving_path = os.path.join('data', 'test', 'predict', 'non-linear', file)
+    predictor.predict_and_save(data=data, saving_path=saving_path, linear=False, model_filename="non-linear-accelerometer.pcl", features="simple", filtering=butter_lowpass_filter, cutoff=2, fs=1000)
 
 for file in accelerometer_gyroscope_files:
-    subprocess.call(f"venv/bin/python predict.py --model-file models/svc_gyroscope.pcl --file {os.path.join('data', 'test', 'transformed', file)} --saving-path {os.path.join('data', 'test', 'predict', 'linear')} --output-filename {file[:-4]} --gyroscope-feature True", shell=True)
-    subprocess.call(f"venv/bin/python predict.py --model-file models/random_forest_gyroscope.pcl --model-type non-linear --file {os.path.join('data', 'test', 'transformed', file)} --saving-path {os.path.join('data', 'test', 'predict', 'non-linear')} --output-filename {file[:-4]} --gyroscope-feature True", shell=True)
+    data = pd.read_csv(os.path.join('data', 'test', 'transformed', file))
+
+    saving_path = os.path.join('data', 'test', 'predict', 'linear', file)
+    predictor.predict_and_save(data=data, saving_path=saving_path, linear=True, model_filename="linear-accelerometer-gyroscope.pcl", features="simple", filtering=butter_lowpass_filter, cutoff=2, fs=1000)
+
+    saving_path = os.path.join('data', 'test', 'predict', 'non-linear', file)
+    predictor.predict_and_save(data=data, saving_path=saving_path, linear=False, model_filename="non-linear-accelerometer-gyroscope.pcl", features="simple", filtering=butter_lowpass_filter, cutoff=2, fs=1000)
+
+for file in accelerometer_feature_files:
+    data = pd.read_csv(os.path.join('data', 'test', 'transformed', file))
+
+    saving_path = os.path.join('data', 'test', 'predict', 'linear-features', file)
+    predictor.predict_and_save(data=data, saving_path=saving_path, model_filename="linear-accelerometer-features.pcl", features="article")
+
+    saving_path = os.path.join('data', 'test', 'predict', 'non-linear-features', file)
+    predictor.predict_and_save(data=data, saving_path=saving_path, model_filename="non-linear-accelerometer-features.pcl", features="article")
+
+for file in accelerometer_gyroscope_features_files:
+    data = pd.read_csv(os.path.join('data', 'test', 'transformed', file))
+
+    saving_path = os.path.join('data', 'test', 'predict', 'linear-features', file)
+    predictor.predict_and_save(data=data, saving_path=saving_path, model_filename="linear-accelerometer-gyroscope-features.pcl", features="article")
+
+    saving_path = os.path.join('data', 'test', 'predict', 'non-linear-features', file)
+    predictor.predict_and_save(data=data, saving_path=saving_path, model_filename="non-linear-accelerometer-gyroscope-features.pcl", features="article")
